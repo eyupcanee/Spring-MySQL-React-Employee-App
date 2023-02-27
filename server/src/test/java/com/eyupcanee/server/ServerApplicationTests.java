601f14dd7672ca8954552a2dde98e8832d1f2fab
eyupcanee <eyupcanee@gmail.com>
2023-02-27 16:55:48 +0300

    project have set

---
+++ server/src/test/java/com/eyupcanee/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyupcanee.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
